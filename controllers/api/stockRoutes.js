07ddf7d63d4d649158e6678bc386d0fb0d19bbaf
kthames <135296109+kthames@users.noreply.github.com>
2023-09-18 20:38:01 -0700

    added stock route and withAuth function

---
+++ controllers/api/stockRoutes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { Stock } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newStock = await Stock.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newStock);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+router.put('/:id', async (req, res) => {
+    // update a stock by its `id` value
+    try{
+      const stockData = await Stock.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json({message: "Stock quantity updated!"});
+    } catch {
+      res.status(400).json(err);
+    }
+  });
+  
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const stockData = await Stock.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!stockData) {
+        res.status(404).json({ message: 'Stock purchase not found!' });
+        return;
+      }
+  
+      res.status(200).json(stockData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
+  
