a98a54fe8aa350b6f80de7787ff7a817114c623b
Francis Yang <fy50167@gmail.com>
2023-09-18 21:08:39 -0700

    Adding server.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
 const stocksRoutes = require('./stockRoutes');
 
-router.use('/users', userRoutes);
 router.use('/stocks', stocksRoutes);
 
 module.exports = router;
