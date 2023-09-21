642686833aaa3843c1cefdb6e7185d72918429d0
kthames <135296109+kthames@users.noreply.github.com>
2023-09-20 19:45:13 -0700

    added homepage routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,43 +2,52 @@ const router = require('express').Router();
 const { Stock, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {
-//   try {
-//     // Get all projects and JOIN with user data
-    
+router.get('/', async (req, res) => {
+  try {
+    // Get all stocks and JOIN with user data
+    const stocksData = await Stock.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
-//     // Serialize data so the template can read it
+    // Serialize data so the template can read it
+    const stocks = stocksData.map((stock) => stock.get({ plain: true }));
 
-//     // Pass serialized data and session flag into template
-//     res.render('homepage', { 
-//       logged_in: req.session.logged_in 
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      stocks,
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get('/project/:id', async (req, res) => {
-//   try {
-//     const projectData = await Project.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
+router.get('/stock/:id', async (req, res) => {
+  try {
+    const stockData = await Stock.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
-//     const project = projectData.get({ plain: true });
+    const stock = stockData.get({ plain: true });
 
-//     res.render('project', {
-//       ...project,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('stock', {
+      ...stock,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
