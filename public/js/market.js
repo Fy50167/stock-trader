038d6db1cb28930b5baa04d49ee114f4f913b18c
kthames <135296109+kthames@users.noreply.github.com>
2023-09-26 14:57:08 -0700

    updated market.js

--- public/js/market.js
+++ public/js/market.js
@@ -1,35 +1,51 @@
+
+
 const userId = parseInt(document.querySelector('#user-id').innerHTML);
 const currentBalance = parseFloat(document.querySelector('#current-balance').innerHTML);
 
 const buyStock = async (event) => {
     const stockId = parseInt(event.currentTarget.nextElementSibling.innerHTML);
-    const stockPrice = parseInt(event.innerHTML);//how do i get to the price value
+    const stockPrice = parseFloat(event.currentTarget.getAttribute('data'));
 
     //logic for if stockPrice is greater than current balance show sweet alert else carry on with the "purchase"
-    
-    
-    const response = await fetch(`/api/stocks/${stockId}`, {
-      method: 'POST',
-      body: JSON.stringify({ user_id: userId }),
+    if (currentBalance < stockPrice) return Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: "Insufficient funds",
+      confirmButtonColor: "#47a0ff",
+    }); else {
+      const response = await fetch(`/api/stocks/${stockId}`, {
+        method: 'POST',
+        body: JSON.stringify({ user_id: userId }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      const newBalance = currentBalance - stockPrice;
+
+      // Send a PUT request to the API endpoint
+    const responsePUT = await fetch('/api/users/balance', {
+      method: 'PUT',
+      body: JSON.stringify({ balance: newBalance }),
       headers: { 'Content-Type': 'application/json' },
     });
 
-    if (response.ok) {
-      Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Stock purchased!',
-        showConfirmButton: false,
-        timer: 1500
-      }); 
-    } else {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: response.statusText,
-        confirmButtonColor: "#47a0ff",
-      });
-    }
+      if (response.ok && responsePUT.ok) {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Stock purchased!',
+          showConfirmButton: false,
+          timer: 1500
+        }); 
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: response.statusText,
+          confirmButtonColor: "#47a0ff",
+        });
+      }
+    }  
 };
 
 const buyButtons = document.getElementsByClassName('buy-button');
